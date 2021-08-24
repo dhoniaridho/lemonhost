75b5c0a6bb6f901a91d8222327ec62d2da5ad739
dhoniaridho <dhoniaridho@gmail.com>
2021-08-24 14:11:02 +0700

    add base feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,9 +7,20 @@
             role="button"
             @click="handleService(service)"
             :class="
-              getService === service ? 'border-red-700 text-red-700' : 'text-gray-400'
+              getService === service
+                ? 'border-red-700 text-red-700'
+                : 'text-gray-400'
             "
-            class="block py-1 lg:inline-block border-b-2 lg:mt-0 px-2 hover:text-red-600 font-semibold"
+            class="
+              block
+              py-1
+              lg:inline-block
+              border-b-2
+              lg:mt-0
+              px-2
+              hover:text-red-600
+              font-semibold
+            "
             v-for="service in services"
             :key="service"
           >
@@ -18,14 +29,23 @@
         </div>
         <div class="mt-5">
           <h1 class="text-5xl font-extrabold">
-            Premium Web <br/> Hosting for Your <br/> Website
+            Premium Web <br />
+            Hosting for Your <br />
+            Website
           </h1>
         </div>
         <div class="my-8 text-gray-500">
-          <p>Blazing fast web hosting for individuals and <br/> businesses of all sizes backed by 24x7x365 Support.</p>
+          <p>
+            Blazing fast web hosting for individuals and <br />
+            businesses of all sizes backed by 24x7x365 Support.
+          </p>
         </div>
         <div class="mt-10">
-          <Link class="text-sm md:text-md mr-2 shadow-red" type="primary" href="/">
+          <Link
+            class="text-sm md:text-md mr-2 shadow-red"
+            type="primary"
+            href="/"
+          >
             Create an Account
           </Link>
           <Link class="text-sm md:text-md mr-2" type="secondary" href="/">
@@ -34,7 +54,49 @@
         </div>
       </div>
       <div class="w-1/2">
-        <img class="object-scale-down" src="https://i.ibb.co/QQ6Gqgn/hero.png" />
+        <img
+          class="object-scale-down"
+          src="https://i.ibb.co/QQ6Gqgn/hero.png"
+        />
+      </div>
+    </section>
+    <section class="flex flex-col px-20 py-20">
+      <div class="flex justify-between mt-10">
+        <div class="w-1/2">
+          <h1 class="text-4xl font-bold">True Cloud Web Hosting</h1>
+          <p class="mt-8">
+            True Cloud Web Hosting All of the hosting packages we offer are
+            deployed instantly on our SSD powered cloud. We don’t use dedicated
+            servers that operate on single pieces of hardware. Our entire
+            infrastructure is built to be reliable, secure, and scalable.
+          </p>
+        </div>
+        <div class="w-1/2 grid grid-cols-3 gap-8">
+          <div>
+            <img src="https://i.ibb.co/pJwRL1K/1.png" alt="">
+          </div>
+          <div>
+            <img src="https://i.ibb.co/pJwRL1K/1.png" alt="">
+          </div>
+          <div>
+            <img src="https://i.ibb.co/pJwRL1K/1.png" alt="">
+          </div>
+          <div>
+            <img src="https://i.ibb.co/pJwRL1K/1.png" alt="">
+          </div>
+          <div>
+            <img src="https://i.ibb.co/pJwRL1K/1.png" alt="">
+          </div>
+          <div>
+            <img src="https://i.ibb.co/pJwRL1K/1.png" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-flow-col">
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
       </div>
     </section>
   </default-layout>
