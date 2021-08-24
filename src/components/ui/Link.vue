d43cd78ce87b88fb02d9b8e736b09e0e23294dbc
dhoniaridho <dhoniaridho@gmail.com>
2021-08-24 08:23:08 +0700

    add base template

---
+++ src/components/ui/Link.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link :class="classNames" class="p-2 rounded-md" :to="href">
+      <slot/>
+  </router-link>
+</template>
+
+<script>
+export default {
+    props: {
+        href: String,
+        type: String
+    },
+    computed: {
+        classNames() {
+            return {
+                'bg-red-700 text-white': this.$props.type === 'primary',
+                'bg-transparent border border-gray-900 hover:bg-red-700 hover:border-red-700 hover:text-white': this.$props.type === 'secondary',
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,55 @@
 <template>
   <default-layout>
-    <div class="home">
-      <h1>{{ numb }}</h1>
-    </div>
+    <section class="h-[calc(100vh-4rem)] flex justify-between place-items-center">
+      <div class="w-1/2">
+        <div class="grid grid-flow-col gap-3">
+          <div
+            role="button"
+            @click="handleService(service)"
+            :class="
+              getService === service ? 'border-red-700' : 'border-transparent'
+            "
+            class="border-b-2"
+            v-for="service in services"
+            :key="service"
+          >
+            {{ service }}
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+        <div class="mt-5">
+          <Link class="text-sm mr-2" type="primary" href="/">
+            Create an Account
+          </Link>
+          <Link class="text-sm mr-2" type="secondary" href="/">
+            Choose your plan
+          </Link>
+        </div>
+      </div>
+      <div></div>
+    </section>
   </default-layout>
 </template>
 
 <script>
 import { onMounted, reactive, toRefs } from "vue";
 import DefaultLayout from "../components/layouts/DefaultLayout.vue";
+import Link from "../components/ui/Link.vue";
 
 export default {
-  components: { DefaultLayout },
+  components: { DefaultLayout, Link },
   name: "Home",
   setup() {
     const state = reactive({
       numb: 0,
+      getService: "",
+      services: ["hosting", "domain", "seo", "email"],
     });
+
+    const handleService = (selected) => {
+      state.getService = selected;
+    };
 
     const increment = () => {
       setInterval(() => {
@@ -28,6 +61,7 @@ export default {
       increment();
     });
     return {
+      handleService,
       ...toRefs(state),
     };
   },
